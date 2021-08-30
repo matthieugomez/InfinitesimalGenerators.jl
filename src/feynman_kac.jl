770d07006ba1d817a67820a4c76f4bbb59dbbb67
matthieugomez <gomez.matthieu@gmail.com>
2021-08-30 12:24:33 -0400

    Update feynman_kac.jl

--- src/feynman_kac.jl
+++ src/feynman_kac.jl
@@ -2,27 +2,27 @@
 With direction = :backward
 Solve the PDE backward in time
 u(x, t[end]) = ψ(x)
-0 = u_t + Tu_t - V(x, t)u +  f(x, t)
+0 = u_t + 𝕋u - v(x, t)u +  f(x, t)
 
 
 With direction = :forward
 Solve the PDE forward in time
 u(x, t[1]) = ψ(x)
-u_t = Tu - V(x)u + f(x)
+u_t = 𝕋u - v(x)u + f(x)
 """
 function feynman_kac(𝕋; 
     t::AbstractVector = range(0, 100, step = 1/12), 
     f::Union{AbstractVector, AbstractMatrix} = zeros(size(𝕋, 1)), 
     ψ::AbstractVector = ones(size(𝕋, 1)),
-    V::Union{AbstractVector, AbstractMatrix} = zeros(size(𝕋, 1)),
+    v::Union{AbstractVector, AbstractMatrix} = zeros(size(𝕋, 1)),
     direction= :backward)
     if direction == :backward
         u = zeros(size(𝕋, 1), length(t))
         u[:, end] = ψ
-        if isa(f, AbstractVector) && isa(V, AbstractVector)
+        if isa(f, AbstractVector) && isa(v, AbstractVector)
             if isa(t, AbstractRange)
                 dt = step(t)
-                B = factorize(I + (Diagonal(V) - 𝕋) * dt)
+                B = factorize(I + (Diagonal(v) - 𝕋) * dt)
                 for i in (length(t)-1):(-1):1
                     ψ = ldiv!(B, u[:, i+1] .+ f .* dt)
                     u[:, i] = ψ
@@ -30,22 +30,22 @@ function feynman_kac(𝕋;
             else
                 for i in (length(t)-1):(-1):1
                     dt = t[i+1] - t[i]
-                    B = I + (Diagonal(V) - 𝕋) * dt
+                    B = I + (Diagonal(v) - 𝕋) * dt
                     u[:, i] = B \ (u[:, i+1] .+ f .* dt)
                 end
             end
-        elseif isa(f, AbstractMatrix) && isa(V, AbstractMatrix)
+        elseif isa(f, AbstractMatrix) && isa(v, AbstractMatrix)
             for i in (length(t)-1):(-1):1
                 dt = t[i+1] - t[i]
-                B = I + (Diagonal(view(V, :, i)) - 𝕋) * dt
+                B = I + (Diagonal(view(v, :, i)) - 𝕋) * dt
                 u[:, i] = B \ (u[:, i+1] .+ f[:, i] .* dt)
             end
         else
-            error("f and V must be Vectors or Matrices")
+            error("f and v must be both AbstractVectors or both AbstractMatrices")
         end
         return u
     elseif direction == :forward
-        u = feynman_kac(𝕋; t = - reverse(t), ψ = ψ, f = f, V = V, direction = :backward)
+        u = feynman_kac(𝕋; t = - reverse(t), ψ = ψ, f = f, v = v, direction = :backward)
         return u[:,end:-1:1]
     else
         error("Direction must be :backward or :forward")
